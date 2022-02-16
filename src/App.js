1fb591f2265e7f8e4afdf8c5cd6ca1e788bb2718
erojas4704 <55060308+erojas4704@users.noreply.github.com>
2022-02-16 02:04:50 -0500

    Added dependencies

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Card, Col, Container, Row } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      Guess it works now.
-      Here are some changes
-    </div>
+    <Container className="App">
+      <Col className="justify-content-center">
+        <div className="display-5">Hello</div>
+        <div>There will be stuff here</div>
+        {/*
+        <Row>
+          <Card>
+            <Card.Img></Card.Img>
+            <Card.Title>Primus</Card.Title>
+            <Card.Body>Ok</Card.Body>
+          </Card>
+          <Card>
+            <Card.Img></Card.Img>
+            <Card.Title>Stocks Game</Card.Title>
+            <Card.Body>Ok</Card.Body>
+          </Card>
+        </Row>
+        */}
+      </Col>
+    </Container>
   );
 }
 
